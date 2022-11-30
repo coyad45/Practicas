eb71f94f45e144f86ba9d264836bd2b559efd15f
coyad45 <coyad45@gmail.com>
2022-11-30 17:02:33 -0300

    Tercer commit: Agregado mas ejercicios

---
+++ Practica 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+            double perimetro;
+            double area;
+            Console.Write("Ingrese el radio:");
+            radio= Convert.ToDouble(Console.ReadLine());
+            perimetro = Math.PI * radio *2;
+            area = Math.PI * radio * radio;
+            Console.WriteLine($"Perimetro:{perimetro}");
+            Console.WriteLine($"Area:{area}");
+
+
+        }
+    }
+}
