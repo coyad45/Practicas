0543aae440323203ad242a7ac7057ea69ae2e84d
coyad45 <coyad45@gmail.com>
2022-11-30 16:48:48 -0300

    Primer commit: iniciando mis practicas

---
+++ practica 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Hola, {nombre}");
+
+        }
+    }
+}
