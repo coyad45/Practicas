2279097cd1492556d1a342e14a67a7b5d4ef9d7a
coyad45 <coyad45@gmail.com>
2022-12-07 03:17:45 -0300

    Quinto commit: Agregando el ejercicio 4 y 5

---
+++ Practica 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float pulgada = 2.54f;
+            float longitud;
+            float resultado;
+            Console.Write("Ingrese longitud: ");
+            longitud = float.Parse(Console.ReadLine());
+            resultado = longitud / pulgada;
+            Console.WriteLine($"{longitud} cm" + " = " + $"{resultado} in");
+
+
+        }
+    }
+}
---
+++ Practica 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            string num;
+            char[] array;
+            Console.Write("Ingrese numero:");
+            numero = int.Parse(Console.ReadLine());
+            num = numero.ToString();         
+            array= num.ToCharArray();
+            Array.Reverse(array);
+            Console.WriteLine(array);
+
+        }
+    }
+}
